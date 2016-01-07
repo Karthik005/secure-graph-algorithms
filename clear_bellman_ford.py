0e67584a61203b829ed94d4bce8d240a5bd75728
Yashvanth <kondi.raptor@gmail.com>
2016-01-07 08:39:17 +0530

    Added a clear version of Bellman-Ford's algorithm to generate a reference solution

---
+++ clear_bellman_ford.py
@@ -0,0 +1,26 @@
+import sys
+
+if __name__ == '__main__':
+	G = [[int(val) for val in line.split()] for line in open('graph')]
+	N = 104059
+	l = 8
+
+	if len(sys.argv) < 2:
+		print "Usage:", sys.argv[0], "<s>"
+		sys.exit(1)
+	s = int(sys.argv[1])
+	
+	p = [0 for i in range(0, len(G))]
+	d = [2**8 for i in range(0, len(G))]
+
+	d[s] = 0
+
+	for i in range(0, len(G)):
+		for e in range(0, len(G)):
+			y = d[e] - d[i] + G[e][i]
+			x = (y+2**l) < 2**l
+			d[i] = d[i] + x*y
+			p[i] = (1-x)*p[i] + x*e
+	
+	print 'Distances:', d
+	print 'Predecessors:', p
